a6354ad7df9665fce837f825f8bfdde88d257cdd
erkay2707 <erkeaiym112707@gmail.com>
2024-07-02 15:50:25 +0600

    add project

---
+++ script.js
@@ -0,0 +1,40 @@
+//1 задание
+//let a =4
+//let b =55
+
+//console.log(a + b)
+
+//let c =4
+//let d =55
+
+//console.log(c * d)
+
+//let e =4
+//let f =55
+
+//console.log(e / f)
+
+//let j =4
+//let h =55
+
+//console.log(j % h)
+
+//2 задание
+//let a='hello '
+//let b='erkay'
+//console.log(a, b)
+
+//3 задание
+// let a = 20
+// let b = 'lalalal'
+// console.log(a * b)
+
+//4 задание
+//let a = 5
+//let b = '5'
+//console.log(10)
+
+//5 задание
+let c = 2
+let b = undefined
+console.log()
